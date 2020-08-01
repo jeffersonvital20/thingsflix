98f3420664572cdc0969a821f3c50267dfcfe627
jefferson.vital <jeffersonvital@gmail.com>
2020-07-31 21:12:35 -0300

    Finalisando aula 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
-import Menu from './components/Menu'
+import Menu from './components/Menu';
+import BannerMain from './components/BannerMain';
+import dadosIniciais from './data/dados_iniciais.json'
+
 
 function App() {
   return (
     <div className="App">
       <Menu/>
+
+      <BannerMain 
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é frontend?"}
+      />
+
+
+
     </div>
   );
 }
