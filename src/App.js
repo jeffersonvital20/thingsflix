8fcf456e392f4761bfcb24fc658f9625eb2999d3
jefferson <jeffersonvital20@gmail.com>
2020-08-01 15:36:05 -0300

    Ajustes da aula ]01

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import Menu from './components/Menu';
 import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 import dadosIniciais from './data/dados_iniciais.json'
 
 
@@ -15,7 +17,32 @@ function App() {
         url={dadosIniciais.categorias[0].videos[0].url}
         videoDescription={"O que é frontend?"}
       />
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
 
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[2]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[3]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[4]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[5]}
+      />      
+
+      <Footer />
 
 
     </div>
